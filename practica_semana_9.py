a2c36ddf5d0c26a5a5a5a12cab14d1981760f325
iZackk__26 <newxstorecr@gmail.com>
2023-04-11 15:33:03 -0600

    Fixes

--- practica_semana_9.py
+++ practica_semana_9.py
@@ -8,14 +8,19 @@ def sucesion(lista):
             return False
         count += 1
     return True
-
+#La solucion matematica se basa en crear una formula en donde se hara el (n + 1) / n, y se compara con el numero de la lista, si son iguales continua hasta terminar el ciclo
 
 def sucesion_answer():
     count = 1
     lista = []
     result = 0
-    while result < 2:
+    while len(lista) < 20:
         result = (count + 1) / count
         lista.append(result)
+        count += 1
     return lista
 print(sucesion_answer())
+
+
+
+

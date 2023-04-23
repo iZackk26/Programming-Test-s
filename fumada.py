ec188a6305cc2768aedaae4cd2574368c0a6af4a
iZackk__26 <newxstorecr@gmail.com>
2023-04-23 00:50:42 -0600

    last changes

--- fumada.py
+++ fumada.py
@@ -62,6 +62,30 @@ def quick_sort(lista):
             big.append(element)
     return quick_sort(small) + [pivot] + quick_sort(big)
 
-y = position_sum(x)
-z = multiplier(y)
-print(quick_sort(z))
+# y = position_sum(x)
+# z = multiplier(y)
+# print(quick_sort(z))
+
+# Ordenamiento por Insercion:
+
+def big(list):
+    big = 0
+    for element in list:
+        if element > big:
+            big = element
+    return big
+
+# print(big([1,2,3,9,8,7,10,8]))
+
+def delete(lista):
+    new = []
+    while len(lista) > 0:
+        bigger = big(lista)
+        new.append(bigger)
+        lista.remove(bigger)
+    return new[::-1]
+
+# print(delete([1,2,3,9,2,7,1,10]))
+
+lista = list(range(20, 0, -1))
+print(lista)
